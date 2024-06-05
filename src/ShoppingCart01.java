88ce66e7fc292798eb525b1dbbb4be40f2d8628c
ALUNO <ALUNO@10.85.198.2>
2024-06-05 14:19:53 -0300

    Commit project Text14

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,17 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String itemDesc = "camisa";
+        String custName = "Alex";
+        String massage = custName + "Quer comprar uma camisa " + itemDesc;
+        //saida
+        System.out.println(massage);
+                
+                
+        // Assign the message variable 
+        
+        
+        // Print and run the code
+        
+    }
+}
